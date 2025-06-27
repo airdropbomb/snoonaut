cb2e4348bcca8cfb299dcfac1ad4dff2d309182a
airdropbomb <saginaw135@gmail.com>
2025-06-27 15:15:32 +0700

    Update snoonaut.js

--- snoonaut.js
+++ snoonaut.js
@@ -25,7 +25,7 @@ const logger = {
   banner: () => {
     console.log(`${colors.cyan}${colors.bold}`);
     console.log('-----------------------------------------------');
-    console.log('  Snoonaut Auto Bot - Airdrop Insiders  ');
+    console.log('  Snoonaut Auto Bot - ADB NODE  ');
     console.log('-----------------------------------------------');
     console.log(`${colors.reset}\n`);
   },
